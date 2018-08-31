933dafc62507bd865b280cc470895aa2bbf3e905
Gideon <gideon@fitanalytics.com>
2018-08-31 16:06:22 +0200

    Create 1-multiplesOf3And5.py

---
+++ python/projectEuler/1-multiplesOf3And5.py
@@ -0,0 +1,11 @@
+# Sum of all the natural numbers below limit that are multiples of 3 or 5
+
+limit = 1000
+n = 1
+count = 0
+while n <= limit - 1:
+    if n % 3 == 0 or n % 5 == 0:
+        count += 1
+    n += 1
+print "Sum of all the natural numbers below", limit, "that are multiples \
+of 3 or 5:", count
