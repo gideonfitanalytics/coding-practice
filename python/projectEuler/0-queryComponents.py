d2df932b5ace9048d5e9c150be37297642379cbe
Gideon <gideon@fitanalytics.com>
2018-10-09 16:13:59 +0200

    Create 0-queryComponents.py

---
+++ python/projectEuler/0-queryComponents.py
@@ -0,0 +1,32 @@
+testcomponent = int(raw_input("Choose An Option\n\n\
+1) check for a given number whether or not it is prime\
+2) test option 2\
+\n: "))
+if testcomponent == 1:
+    isprime = 0
+    number = int(raw_input("enter a number you would like to check\
+    \n: "))
+    if number >= 1:
+        if number == 2:
+            isprime = 1
+        elif number % 2 != 0:
+            if number == 3:
+                isprime = 1
+            elif number % 3 != 0:
+                isprime = 1
+                for n in range(4, number / 2 + 1):
+                    if number % n == 0:
+                        isprime = 0
+            else:
+                print "error: unexpected else"
+    else:
+        isprime = 0
+    if isprime == 0:
+        print "the given number is not prime"
+    else:
+        print "the given number is prime"
+elif testcomponent == 2:
+    print "option 2"
+else:
+    print "not a valid option"
+raw_input("press ENTER to close program")
